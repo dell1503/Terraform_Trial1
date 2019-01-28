3a7439273ceacf899a030a301a34cacfd17ae2da
dell1503 <dell1503@users.noreply.github.com>
2019-01-28 17:12:02 +0100

    ADD SG and VPC

--- Bitcoin_node.tf
+++ Bitcoin_node.tf
@@ -1,15 +1,38 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+# VPC
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+#SecurityGroup
+resource "aws_security_group" "default" {
+  name        = "default"
+  description = "default group, create SSH open"
+  vpc_id      = "${resource.aws_vpc.mainvpc}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 #EC2
 resource "aws_instance" "bitcoinnode" {
   ami           = "ami-0cf8fa6a01bb07363"                                             #Ubuntu18.04 LTS,hvm:ebs-ssd
   instance_type = "t2.micro"
+  security_groups = [
+        "${aws_security_group.first.id}"
+    ]
   tags = {
     Name        = "EC2"
     Environment = "Dev"
   }
   provisioner "local-exec" {
-    #command = "wget https://raw.github.com/XertroV/BitcoinAutoNode/master/bitcoinAutoNode.sh ; bash bitcoinAutoNode.sh"
+    #command = "https://raw.githubusercontent.com/dell1503/BitcoinAutoNode/master/bitcoinAutoNode.sh"
   }
 }
