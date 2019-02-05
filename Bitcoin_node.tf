f39c5ed796a066211aaaab9f362cc229110bf1de
Sebastian Bryner <brynersa@gmail.com>
2019-02-05 18:00:37 +0100

    add Bitcoin Port to SG.

--- Bitcoin_node.tf
+++ Bitcoin_node.tf
@@ -25,6 +25,12 @@ resource "aws_security_group" "ec2bitcoinnode" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    from_port   = 8333
+    to_port     = 8333
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-bucket-dell1503"
-    key    = "path/to/my/key"
+    key    = "dell1503/terraform-state-bucket.tfstate"
     region = "eu-central-1"
   }
 }
